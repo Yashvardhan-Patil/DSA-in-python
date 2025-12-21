268f4b22c34e1b886b7764d7db8221fc6a8d5b91
Yashvardhan-Patil <yashpatil6001@gmail.com>
2025-12-21 10:12:30 +0530

    Delete node from given list

--- Matrix/Linkedlist/delete from sll.py
+++ Matrix/Linkedlist/delete from sll.py
@@ -1,31 +1,5 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-
-class sll:
-    def __init__(self):
-        self.head = None
-    def Delete(self,val):
-        temp=self.head
-        if temp.val==val:
-            self.head=temp.next
-            del temp
-            return
-        else:
-            found=False
-            prev=None
-            while temp is not None:
-                if temp.val==val:
-                    found=True
-                    break
-                temp=temp.next
-                prev=temp
-            if found:
-                prev.next=temp.next
-                return
-            else:
-                print("Element Not Found")
-
-            
+class Solution(object):
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
         
