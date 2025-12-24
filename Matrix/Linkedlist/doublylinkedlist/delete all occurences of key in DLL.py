619922e4b07e522ab8905d401b9a1d598a4bf9ac
Yashvardhan-Patil <yashpatil6001@gmail.com>
2025-12-24 21:59:00 +0530

    Dsa in python

---
+++ Matrix/Linkedlist/doublylinkedlist/delete all occurences of key in DLL.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeNthFromEnd(self, head,key):
+        temp=head
+        curr=None
+        new_node=None
+        
+        while temp is not None:
+            if temp.val==key:
+                if curr is not None:
+                    curr.next=temp.next
+                if temp.next is not None:
+                    temp.next.prev=curr
+                if temp==new_node:
+                    new_node=new_node.next
+            curr=temp
+            temp=temp.next
+        return new_node
+            
+
